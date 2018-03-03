ce8f3a277543f94e0bb8fbf0a2882d15fd0e976c
Tyson Whitehead <twhitehead@gmail.com>
2018-03-03 16:58:49 -0500

    Print command line usage (closes #1)

--- igc-callsign.sh
+++ igc-callsign.sh
@@ -1,1 +1,7 @@
+if (($# != 1))
+then
+  echo "Usage: $0 <igc file>"
+  exit 1
+fi
+
 grep ^HFGIDGLIDERID: $1 | cut -c 15-
--- igc-height.sh
+++ igc-height.sh
@@ -1,1 +1,7 @@
+if (($# != 1))
+then
+  echo "Usage: $0 <igc file>"
+  exit 1
+fi
+
 grep ^B $1 | cut -c 31-35 | sort | tail -n 1
